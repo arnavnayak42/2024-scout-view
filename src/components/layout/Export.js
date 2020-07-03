a82150c19c3eae75ddc85a87b161c24dcdc1cc47
Abhik Ray <abhikaboy@gmail.com>
2020-07-02 20:02:57 -0400

    remove big chunger

--- src/components/layout/Export.js
+++ src/components/layout/Export.js
@@ -1,63 +1,67 @@
-import React, { Component } from 'react'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
-var QRCode = require('qrcode.react');
+import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+var QRCode = require("qrcode.react");
 export class Export extends Component {
-    render() {
-        return (
-            <div>
-                <header><h3 style = {headerStyle}>Export</h3></header>
-                <Row style = {fs}> <div style = {topSpace}></div> </Row>
-                <Container fluid style = {middle}>
-                    <Row>
-                        <QRCode value={JSON.stringify(this.props.data)} style = {big}/>
-                    </Row>
-                    <Row style = {fs}> <div style = {topSpace}></div> </Row>
-                </Container>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <header>
+          <h3 style={headerStyle}>Export</h3>
+        </header>
+        <Row style={fs}>
+          {" "}
+          <div style={topSpace}></div>{" "}
+        </Row>
+        <Container fluid style={middle}>
+          <Row>
+            <QRCode value={JSON.stringify(this.props.data)} style={big} />
+          </Row>
+          <Row style={fs}>
+            <div style={topSpace}></div>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 const topSpace = {
-    marginTop: '25vh',
-    width:'100%'
-}
+  marginTop: "25vh",
+  width: "100%",
+};
 const headerStyle = {
-    textAlign: 'center',
-    fontSize: "2.5em",
-    padding: "1em",
-    paddingBottom: '20px',
-    paddingTop: '20px',
-    fontFamily: "Code",
-    backgroundColor: "#ff5555",
-    color: "white"
-}
+  textAlign: "center",
+  fontSize: "2.5em",
+  padding: "1em",
+  paddingBottom: "20px",
+  paddingTop: "20px",
+  fontFamily: "Code",
+  backgroundColor: "#ff5555",
+  color: "white",
+};
 const subHead = {
-    textAlign: 'center',
-    fontSize: "2em",
-    padding: "1em",
-    paddingBottom: '20px',
-    paddingTop: '20px',
-    fontFamily: "Code",
-    color: "white" 
-}
+  textAlign: "center",
+  fontSize: "2em",
+  padding: "1em",
+  paddingBottom: "20px",
+  paddingTop: "20px",
+  fontFamily: "Code",
+  color: "white",
+};
 const middle = {
-    textAlign: 'center',
-    margin: 'auto'
-}
+  textAlign: "center",
+  margin: "auto",
+};
 const big = {
-    textAlign: 'center',
-    width: '20%',
-    height: '20%',
-    margin: 'auto',
-    borderStyle: 'solid'
-
-}
+  textAlign: "center",
+  margin: "auto",
+  borderStyle: "solid",
+};
 const fs = {
-    height: '100%',
-    width: '100%'
-}
-export default Export
+  height: "100%",
+  width: "100%",
+};
+export default Export;
