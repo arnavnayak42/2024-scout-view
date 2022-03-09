e634dc5e827e0a6b3934184b17b446b32bf52e5a
Abhik Ray <abhikray@Abhiks-MacBook-Air.local>
2022-03-08 19:54:15 -0500

    finally got rid of chakra

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { ChakraProvider } from '@chakra-ui/react';
-
 ReactDOM.render(
 	<React.StrictMode>
 		{/* <ChakraProvider> */}
