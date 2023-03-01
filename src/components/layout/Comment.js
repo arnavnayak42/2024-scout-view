d240f7b16cdb7a505d7e110f38111fddfdab3f7a
codejvn <65090247+codejvn@users.noreply.github.com>
2023-03-01 16:15:32 -0500

    cleaning up comment.js

--- src/components/layout/Comment.js
+++ src/components/layout/Comment.js
@@ -3,36 +3,58 @@ import InputGroup from 'react-bootstrap/InputGroup'
 import FormControl from 'react-bootstrap/FormControl'
 import App from '../../App.js'
 export class Comment extends Component {//style={size}
-    render(props) {
+    // constructor(props){
+    //     super(props);
+    //     this.state = {clear: false};
+    // } 
+    // static getDerivedStateFromProps(props, state){
+    //     return {clear: props.redo};
+    // }
+    // componentDidMount(){
+    //     if(this.state.clear == true){
+    //         this.forceUpdate();
+    //         console.log("updating again");
+    //         console.log(this.state.clear);
+    //         this.setState({clear: false});
+    //     }
+    //     else{
+    //         console.log("i mean it was called");
+    //     }
+        
+    // }
+    render() {
         console.log("trying to render comment")
-        console.log(this.props.curTab)/**  
+        console.log(this.props.curTab)
+        // console.log("current state of comment");
+        // console.log(this.state.clear);
+        /**  
         if(getTab == 2){//this doesn't work because App object isn't created 
             console.log("we're in teleop");*/
             //if(App.getTab() == 2){console.log("finally this works")}
             //if(props.curTab == 2){console.log("finally this works")}
-        if(this.props.curTab==2){
-        return (
-                <><div style={topMargin}/>
-                <div className="comment">
-                    <p>Comment</p>
-                    <InputGroup onChange={this.props.sendComment} type="text">
-                        <FormControl />
-                    </InputGroup>
-                </div></>
-            ) 
-        }
-        else{
+        // if(this.props.curTab==2){
+        // return (
+        //         <><div style={topMargin}/>
+        //         <div className="comment">
+        //             <p>Comment</p>
+        //             <InputGroup onChange={this.props.sendComment} type="text">
+        //                 <FormControl />
+        //             </InputGroup>
+        //         </div></>
+        //     ) 
+        // }
+        // else{
             return (
                 <><div style={topMargin}/>
                 <div style={size}>
                     <h4>Comment</h4>
                     <p>i.e if they can intake from the grid, did they score many links, etc. etc.</p>
-                    <InputGroup onChange={this.props.sendComment} type="text">
-                        <FormControl />
+                    <InputGroup onChange={this.props.sendComment} type='text'>
+                        <FormControl className='textField'/>
                     </InputGroup>
                 </div></>
             )
-        }
+        // }
         
 }
 }
